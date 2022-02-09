d1c7b358b833695728332e84f14bb1b8318b72e8
Mauricio <mauriciomeza95@hotmail.com>
2022-02-09 12:33:36 -0600

    Desarrollo de patrón de autentificación terminado

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,22 @@
-const res = require("express/lib/response")
+// ./controllers/indexController.js
 
-// 1. CONTROLLER
+// 1. CONTROLLERS
 
 exports.getHome = (req, res) => {
-    res.render("index")
+
+	res.render("index")
+
 }
 
 exports.getProfile = (req, res) => {
-    res.render("profile")
+
+	// console.log(req.session.currentUser?.username)
+
+	// // SOLUCIÓN 1
+	// const username = req.session.currentUser?.username || ""
+	// const email = req.session.currentUser?.email || ""
+	// const msg = req.session.currentUser?.msg || "" 
+
+	res.render("profile")
+
 }
--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,24 @@ const router		= express.Router()
 
 const authController		= require("./../controllers/authController")
 
+const routeGuard			= require("./../middlewares/route-guard")
+
 // 2. ROUTER
 // A1. SIGNUP - OBTENER PÁGINA
-router.get("/register", authController.register)
+router.get("/register", routeGuard.authAreas, authController.register)
 
-// A2. SIGNUP - ENVIAR FORMULARIO
-router.post("/register", authController.registerForm)
+// A2. SIGNUP - ENVIAR FORMULARIO DE SIGNUP
+router.post("/register", routeGuard.authAreas, authController.registerForm)
 
-// B. LOGIN - OBTENER PÁGINA
-router.get("/login", authController.login)
+// B1. LOGIN - OBTENER PÁGINA
+router.get("/login", routeGuard.authAreas, authController.login)
+
+// B2. LOGIN - ENVIAR FORMULARIO DE LOGIN
+router.post("/login", routeGuard.authAreas, authController.loginForm)
+
+
+// C1. SIGNOUT - CERRAR SESIÓN
+router.get("/logout", authController.logout)
 
 
 
